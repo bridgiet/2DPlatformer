923dc17744ffb8ec51da010d96cf58a1d9aa8d2f
bridgiet <bjosep24@my.centennialcollege.ca>
2016-02-23 19:51:40 -0500

    Added files via upload
    the player can move left and right
    animations for the player is done
    ground and background's basic coding is completed

---
+++ Pokemon/Assets/scripts/MeouthController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeouthController : MonoBehaviour {
+    //PRIVATE INSTANCE VARIABLES
+    private Animator _animator;
+    private float _move;
+    private float _jump;
+    private bool _facingRight;
+    private Transform _transform;
+	// Use this for initialization
+	void Start () {
+        this._transform = gameObject.GetComponent<Transform>();
+        this._animator = gameObject.GetComponent<Animator>();
+        this._move = 0f;
+        this._jump = 0f;
+        this._facingRight = true; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this._move = Input.GetAxis("Horizontal");
+        this._jump = Input.GetAxis("Vertical");
+        Debug.Log("WALK" + this._move);
+        Debug.Log("JUMP" + this._jump);
+        if(this._move != 0)
+        {
+
+            if (this._move > 0)
+            {
+                this._facingRight = true;
+            }
+            if (this._move < 0)
+            {
+                this._facingRight = false;
+            }
+            this._flip();
+            //call the walk clip
+            this._animator.SetInteger("AnimeState", 0);
+        }    
+        if(this._jump > 0)
+        {
+            //call the jump clip
+            this._animator.SetInteger("AnimeState", 1);
+        }
+	}
+
+    //PRIVATE METHOD
+    private void _flip()
+    {
+        if (this._facingRight)
+        {
+            this._transform.localScale = new Vector2(1, 1);
+        }
+        else
+        {
+            this._transform.localScale = new Vector2(-1, 1);
+        }
+    }
+}
