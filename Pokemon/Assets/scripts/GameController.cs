04ff7ed7ed5d929fb39d831f6cbee4bdd84637b1
bridgiet <bjosep24@my.centennicalcollege.ca>
2016-02-26 04:36:31 -0500

    Respawn on death and Lives and Scoring Label Added

---
+++ Pokemon/Assets/scripts/GameController.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class GameController : MonoBehaviour {
+    //PRIVATE INSTANCE VARIABLES
+    private int _scoreValue;
+    private int _livesValue;
+
+    //PUBLIC INSTANCE VARIABLES
+    public Text livesLabel;
+    public Text scoreLabel;
+    public Text gameOverLabel;
+    public Text highSchoolLabel;
+    public Button restartButton;
+
+    //PUBLIC ACCESS METHODS
+    public int ScoreValue
+    {
+        get
+        {
+            return this._scoreValue;
+        }
+
+        set
+        {
+            this._scoreValue = value;
+            this.scoreLabel.text = "Score: " + this._scoreValue;
+        }
+    }
+
+    public int LivesValue
+    {
+        get
+        {
+            return this._livesValue;
+        }
+
+        set
+        {
+            this._livesValue = value;
+            if (this._livesValue <= 0)
+            {
+                _endGame();
+            }
+            else
+            {
+                this.livesLabel.text = "Lives: " + this._livesValue;
+               
+            }           
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        this._initialize();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //PRIVATE METHODS
+    private void _initialize()
+    {
+        this.ScoreValue = 0;
+        this.LivesValue = 5;
+        this.gameOverLabel.gameObject.SetActive(false);
+        this.highSchoolLabel.gameObject.SetActive(false);
+        this.restartButton.gameObject.SetActive(false);
+    }
+
+    private void _endGame()
+    {
+        this.highSchoolLabel.text = "High Score: " + _scoreValue;
+        this.gameOverLabel.gameObject.SetActive(true);
+        this.highSchoolLabel.gameObject.SetActive(true);
+        this.scoreLabel.gameObject.SetActive(false);
+        this.livesLabel.gameObject.SetActive(false);
+        this.restartButton.gameObject.SetActive(true);
+    }
+
+    //PUBLIC METHODS
+    public void RestartButtonClick()
+    {
+        Application.LoadLevel("Main");
+    }
+}
---
+++ Pokemon/Assets/scripts/MeowthController.cs
@@ -0,0 +1,144 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.scripts;
+
+public class MeowthController : MonoBehaviour {
+    //PUBLIC INSTANCE VARIABLES
+    public VelocityRange velocityRange;
+    public float moveForce;
+    public float jumpForce;
+    public Transform groundCheck;
+    public Transform camera;
+    public GameController gameController;
+
+    //PRIVATE INSTANCE VARIABLES
+    private Animator _animator;
+    private float _move;
+    private float _jump;
+    private bool _facingRight;
+    private Transform _transform;
+    private Rigidbody2D _rigidBody2d;
+    private bool _isGrounded;
+	
+    // Use this for initialization
+	void Start () {
+        //Initialize Public Instance Variables
+        this.velocityRange = new VelocityRange(750f, 20000f);
+        this.moveForce = 750f;
+        this.jumpForce = 20000f;
+        
+        //Initialize Private Instance Variables
+        this._transform = gameObject.GetComponent<Transform>();
+        this._rigidBody2d = gameObject.GetComponent<Rigidbody2D>();
+        this._animator = gameObject.GetComponent<Animator>();
+        this._move = 0f;
+        this._jump = 0f;
+        this._facingRight = true;
+
+        //Place the hero in the strting position
+        this._spawn();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 currentPosition = new Vector3(this._transform.position.x, this._transform.position.y, this.camera.position.z);
+        this.camera.position = currentPosition;
+        this._isGrounded = Physics2D.Linecast(
+                            this._transform.position, 
+                            this.groundCheck.position,
+                            1 << LayerMask.NameToLayer("Ground"));
+        Debug.DrawLine(this._transform.position, this.groundCheck.position);
+
+        float forceX = 0f;
+        float forceY = 0f;
+
+        //get absolute value of velocity for our gameObject
+        float absVelX = Mathf.Abs(this._rigidBody2d.velocity.x);
+        float absVelY = Mathf.Abs(this._rigidBody2d.velocity.y);
+
+        //Ensure the player is grounded before any movement check
+        if (this._isGrounded)
+        {
+            //gets a number between -1 to 1 for both Horizontal and Vertical axes
+            this._move = Input.GetAxis("Horizontal");
+            this._jump = Input.GetAxis("Vertical");
+
+            if (this._move != 0)
+            {
+                if (this._move > 0)
+                {
+                    //Movement force
+                    if (absVelX < this.velocityRange.maximum)
+                    {
+                        forceX = this.moveForce;
+                    }
+                    this._facingRight = true;
+                }
+                if (this._move < 0)
+                {
+                    //Movement Force
+                    if (absVelX < this.velocityRange.maximum)
+                    {
+                        forceX = -this.moveForce;
+                    }
+                    this._facingRight = false;
+                }
+                this._flip();
+
+                //call the walk clip
+                this._animator.SetInteger("AnimeState", 1);
+            }
+            else
+            {
+                //call the idle clip
+                this._animator.SetInteger("AnimeState", 0);
+            }
+
+            
+
+            if (this._jump > 0)
+            {
+                //Jump force
+                if (absVelY < this.velocityRange.maximum)
+                {
+                    forceY = this.jumpForce;
+                }
+            }
+        }    
+        else
+        {
+            //call the jump clip
+            this._animator.SetInteger("AnimeState", 2);
+        }
+
+        //Apply the forces to the player
+        this._rigidBody2d.AddForce(new Vector2(forceX, forceY));
+	}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Death"))
+        {
+            this.gameController.LivesValue--;     
+            this._spawn();
+        }
+    }
+
+    //PRIVATE METHOD
+    private void _flip()
+    {
+        if (this._facingRight)
+        {
+            this._transform.localScale = new Vector2(1, 1);
+        }
+        else
+        {
+            this._transform.localScale = new Vector2(-1, 1);
+        }
+    }
+
+    private void _spawn()
+    {
+        this._transform.position = new Vector3(-110, 720, 0);
+    }
+}
